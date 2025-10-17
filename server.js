cfd25270fea5665a7c2f045f7e6e0ec9f7422111
hanaaden <hanaadamabdi@gmail.com>
2025-10-17 13:40:06 +0300

    updates

--- server.js
+++ server.js
@@ -1,42 +1,44 @@
 const express = require("express")
 const app = express()
-
 app.use(express.json())
 
-let users =[
+let users = [
     {id: 1 , name : "Jane"},
     {id: 2 , name : "John"}
 ]
-app.get("/users" , (req , res) =>{
+
+
+app.get("/users" , (req ,res)=>{
     res.json(users)
 })
 
-
-app.post("/users" , (req , res)=>{
+app.post("/users" , (req ,res)=>{
     const newUser = {
         id : users.length + 1 ,
         name : req.body.name
-    };
-    users.push(newUser);
+    }
+    users.push(newUser)
     res.status(201).json(newUser)
 })
 
-app.put("/users/:id" , (req ,res)=>{
+app.put("/users:id" , (req ,res)=>{
     const userId = parseInt(req.params.id)
     const user = users.find(u=>u.id === userId)
-    if(!user) return res.status(404).json({message : "user not found"})
-
+    if(!user){
+    return res.status(404).json({messege : "user not found"})
+    } 
     user.name = req.body.name
     res.json(user)
 })
 
-app.delete("/users/:id" , (req,res)=>{
+app.delete("/users/:id" ,(req ,res)=>{
     const userId = parseInt(req.params.id)
     const userIndex = users.findIndex(u => u.id === userId)
+    if(userId === -1){
+        return res.status(303).json({messege : "user not found"})
 
-    if(userId === -1 ) return res.status(404).json({message :"user not found"})
-    
-    users.splice(userIndex , 1 )
-    res.json({message :"user deleted successfully"})
+    }
+    users.splice(userIndex , 1)
+    res.json({messege : "deleted successfully"})
 })
-app.listen(5000 , ()=>console.log("the server is running on port 5000 "))
+app.listen(5000, ()=> console.log("server is running on port 5000"))
